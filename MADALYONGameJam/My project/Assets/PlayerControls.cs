285c68b2b5bf6b47c666bf063a35071b796c5495
mehmetcalistirir <mehmetcalistirir@gmail.com>
2025-12-06 00:52:39 +0300

    YurumeSesi

--- MADALYONGameJam/My project/Assets/PlayerControls.cs
+++ MADALYONGameJam/My project/Assets/PlayerControls.cs
@@ -117,7 +117,7 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                 {
                     ""name"": ""up"",
                     ""id"": ""48aebc1b-74b2-4dc9-a33d-1c708bd9d77f"",
-                    ""path"": ""<Joystick>/stick/up"",
+                    ""path"": ""<Keyboard>/w"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -128,7 +128,7 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                 {
                     ""name"": ""down"",
                     ""id"": ""7c7dae9d-dc94-407c-8d85-d5a04344f70b"",
-                    ""path"": ""<Joystick>/stick/down"",
+                    ""path"": ""<Keyboard>/s"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -139,7 +139,7 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                 {
                     ""name"": ""left"",
                     ""id"": ""2fb25903-3f7b-4418-8eb8-2657da229c69"",
-                    ""path"": ""<Joystick>/stick/left"",
+                    ""path"": ""<Keyboard>/a"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -150,7 +150,7 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                 {
                     ""name"": ""right"",
                     ""id"": ""de685c44-87c9-4534-ab84-88c389a03699"",
-                    ""path"": ""<Joystick>/stick/right"",
+                    ""path"": ""<Keyboard>/d"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
--- MADALYONGameJam/My project/Assets/Scripts/Player/PlayerMovement.cs
+++ MADALYONGameJam/My project/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,9 +5,17 @@ public class PlayerMovement2D : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
+    public AudioSource footstepSource;
+    public AudioClip lightFootstepLoop;   // 0–5 piyon
+    public AudioClip heavyFootstepLoop;   // >5 piyon
+    public int pawnCount = 0;
+    private PlayerPiyon playerPiyon; 
+
+
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private PlayerControls controls;
+       
 
     void Awake()
     {
@@ -29,10 +37,54 @@ public class PlayerMovement2D : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        footstepSource.loop = true;
+        playerPiyon = FindObjectOfType<PlayerPiyon>();
+    }
+  void Update()
+    {
+       pawnCount = playerPiyon.GetCount();
+
+    Debug.Log("Pawn Count = " + pawnCount);
+
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+        HandleFootstepSound();
+    }
+
+    void HandleFootstepSound()
+    {
+        Debug.Log("Pawn Count = " + pawnCount);
+
+        bool isMoving = moveInput != Vector2.zero;
+
+        if (isMoving)
+        {
+            // Piyon sayısına göre hangi loop çalacak?
+            AudioClip targetClip = (pawnCount > 5) ? heavyFootstepLoop : lightFootstepLoop;
+
+            // Eğer yanlış clip çalıyorsa, doğru olanla değiştir
+            if (footstepSource.clip != targetClip)
+            {
+                footstepSource.clip = targetClip;
+                footstepSource.Play();
+            }
+
+            // Eğer hiç çalmıyorsa başlat
+            if (!footstepSource.isPlaying)
+            {
+                footstepSource.Play();
+            }
+        }
+        else
+        {
+            // Hareket durunca sesi anında kes
+            if (footstepSource.isPlaying)
+            {
+                footstepSource.Stop();
+            }
+        }
     }
 }
